feba1be8d5047a5164f098b11b6287b7a7153673
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-01-16 22:52:19 +0530

    Setup connection with db and added server responses

--- server/server.js
+++ server/server.js
@@ -3,15 +3,29 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const os = require('os');
+const mongoose = require('mongoose');
 
 // Route imports
 const mainRouter = require('./routes/main');
 
 const PORT = process.env.SERVER_PORT || 5000;
 
+// Connecting to the database
+(async () => {
+  try {
+    await mongoose.connect(process.env.TEST_DATABASE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to DB successfully');
+  } catch (error) {
+    console.log('Failed to establish connection with DB', error);
+  }
+})();
+
 const app = express();
 
-// Adding the routes
+// Adding the routes to server
 app.use('/', mainRouter);
 
 const server = app.listen(PORT, () => {
