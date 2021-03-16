95bf8fd95644a58365febfcaac5ce13f2e86e955
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-03-16 12:53:25 +0530

    Removed google-auth-library, removed signup, login with google and removed googel auth utility function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,105 +6,13 @@ const {
   getUserNameToken,
   getAccessToken,
 } = require('../utils/auth');
-const googleAuth = require('../utils/googleAuth');
 const githubAuth = require('../utils/githubAuth');
 const SERVER_RESPONSE = require('../utils/serverResponses');
 
 // Sign up new user to grupo
 const signUpUser = (req, res) => {
   try {
-    if (req.body.issuer === 'google') {
-      googleAuth(req.body.accessToken)
-        .then((data) => {
-          User.find({ email: data.email })
-            .exec()
-            /* eslint-disable consistent-return */
-            .then((user) => {
-              if (user.length === 1) {
-                return res.status(409).json({
-                  status: false,
-                  payload: {
-                    message: SERVER_RESPONSE.CONFLICT,
-                  },
-                });
-              }
-              // Extracting usernmae from email
-              let username = data.email.match(/^([^@]*)@/)[1];
-              User.find({ username })
-                .exec()
-                .then((userCheck) => {
-                  if (userCheck.length !== 0) {
-                    username += userCheck.length;
-                  }
-                })
-                .catch((error) => {
-                  console.log(error);
-                  res.status(500).json({
-                    status: false,
-                    payload: {
-                      message: SERVER_RESPONSE.ERROR,
-                    },
-                  });
-                });
-              bcrypt.hash(req.body.password, 10, (error, hashedPassword) => {
-                if (error) {
-                  console.log(error);
-                  throw new Error('Password encryption failed');
-                }
-
-                const newUser = new User({
-                  username: username,
-                  firstname: data.given_name,
-                  lastname: data.family_name,
-                  email: data.email,
-                  password: hashedPassword,
-                  issuer: req.body.issuer,
-                  signupType: req.body.signupType,
-                  profilePic: data.picture,
-                });
-
-                console.log(newUser);
-
-                newUser
-                  .save()
-                  .then(() => {
-                    res.status(201).json({
-                      status: true,
-                      payload: {
-                        message: SERVER_RESPONSE.CREATED,
-                      },
-                    });
-                  })
-                  .catch(() => {
-                    res.status(406).json({
-                      status: false,
-                      payload: {
-                        message: SERVER_RESPONSE.MISSING,
-                      },
-                    });
-                  });
-              });
-            })
-            .catch((error) => {
-              console.log(error);
-              res.status(500).json({
-                status: false,
-                payload: {
-                  message: SERVER_RESPONSE.ERROR,
-                },
-              });
-            });
-        })
-        .catch((error) => {
-          console.log(error);
-          res.status(401).json({
-            status: false,
-            payload: {
-              message: SERVER_RESPONSE.ERRORTOKEN,
-            },
-          });
-        });
-    } else if (req.body.issuer === 'github') {
+    if (req.body.issuer === 'github') {
       githubAuth(req.body.accessCode)
         .then((response) => {
           User.find({ email: response.email })
@@ -135,8 +43,6 @@ const signUpUser = (req, res) => {
                   profilePic: response.avatar_url,
                   profileLink: response.html_url,
                 });
-
-                console.log(newUser);
 
                 newUser
                   .save()
@@ -197,61 +103,12 @@ const signUpUser = (req, res) => {
 // Login user to grupo
 const loginUser = (req, res) => {
   try {
-    if (req.body.issuer === 'google') {
-      googleAuth(req.body.accessToken)
-        .then((data) => {
-          User.find({ email: data.email })
-            .exec()
-            .then((user) => {
-              console.log(user);
-              if (user.length >= 1) {
-                res.cookie(
-                  'grupo_rtk',
-                  getRefreshToken(user[0]),
-                  getCookieOptions(604800000)
-                );
-                res.cookie(
-                  'grupo_u',
-                  getUserNameToken(user[0]),
-                  getCookieOptions(604800000)
-                );
-                res.status(200).json({
-                  status: true,
-                  payload: {
-                    message: SERVER_RESPONSE.LOGIN,
-                    accessToken: getAccessToken(user[0]),
-                  },
-                });
-              } else {
-                res.status(403).json({
-                  status: false,
-                  payload: { message: SERVER_RESPONSE.REGISTER },
-                });
-              }
-            })
-            .catch((error) => {
-              console.log(error);
-              res.status(500).json({
-                status: false,
-                payload: { message: SERVER_RESPONSE.ERROR },
-              });
-            });
-        })
-        .catch((error) => {
-          console.log(error);
-          res.status(401).json({
-            status: false,
-            payload: { message: SERVER_RESPONSE.ERRORTOKEN },
-          });
-        });
-    } else if (req.body.issuer === 'github') {
-      console.log(req.body.accessCode);
+    if (req.body.issuer === 'github') {
       githubAuth(req.body.accessCode)
         .then((response) => {
           User.find({ email: response.email })
             .exec()
             .then((user) => {
-              console.log(user);
               if (user.length >= 1) {
                 res.cookie(
                   'grupo_rtk',
@@ -310,6 +167,7 @@ const loginUser = (req, res) => {
   }
 };
 
+// Logout user from grupo
 const logoutUser = (req, res) => {
   try {
     res.clearCookie('grupo_rtk');
